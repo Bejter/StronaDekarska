22df3d858395d2fc596fe2c295be2f937f6323a4
Bejter <kacper.majda62@gmail.com>
2026-08-03 08:28:49 +0200

    Initialized frontend(React, TypesScript, Tailwind)

---
+++ frontend/src/App.tsx
@@ -0,0 +1,14 @@
+import './App.css'
+
+function App() {
+  return (
+    <main className="min-h-screen bg-slate-950 text-white flex items-center justify-center">
+      <h1 className="text-5xl font-bold text-amber-400">
+        Usługi dekarskie
+      </h1>
+    </main>
+  );
+}
+
+
+export default App
