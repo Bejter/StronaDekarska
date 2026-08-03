fb5f889389b02ba83adfa3138b6a45283a735b70
Bejter <kacper.majda62@gmail.com>
2026-08-03 09:17:20 +0200

    test

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -4,7 +4,7 @@ function App() {
   return (
     <main className="min-h-screen bg-slate-950 text-white flex items-center justify-center">
       <h1 className="text-5xl font-bold text-amber-400">
-        Usługi dekarskie
+        Usługi dekarskie asd
       </h1>
     </main>
   );
